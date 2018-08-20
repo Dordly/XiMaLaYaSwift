dc1bb0a5b6c0ba9333151faadc6f7c19b73f6daf
Dordly <15730128494@163.com>
2018-08-20 12:24:51 +0800

    新增工程

---
+++ XiMaLaYaSwift/XiMaLaYaSwift/Other/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XiMaLaYaSwift
+//
+//  Created by dordly on 2018/8/20.
+//  Copyright © 2018年 dordly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
