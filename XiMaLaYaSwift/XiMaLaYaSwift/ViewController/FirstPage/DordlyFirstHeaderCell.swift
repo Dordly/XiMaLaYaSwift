05d874744850a394767d24d0338898e3895f8a7c
Dordly <15730128494@163.com>
2018-08-24 14:20:38 +0800

    增添图标

--- XiMaLaYaSwift/XiMaLaYaSwift/ViewController/FirstPage/DordlyFirstHeaderCell.swift
+++ XiMaLaYaSwift/XiMaLaYaSwift/ViewController/FirstPage/DordlyFirstHeaderCell.swift
@@ -9,5 +9,15 @@
 import UIKit
 
 class DordlyFirstHeaderCell: UICollectionViewCell {
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = MainBgColor
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
